ee7cf9b7fe6da1746d1b3e4400f007ade260e63b
Kajol Khairnar <74052505+kajolkhairnar@users.noreply.github.com>
2022-05-12 08:24:19 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+                <div class="clear"></div>
+            </div>
+            <!-- // #container -->
+        </div>	
+        <!-- // #containerHolder -->
+        
+        <p id="footer">Hospital Bed Management System...#CodingArmy</p>
+    </div>
+    <!-- // #wrapper -->
+</body>
+</html>
